570002f0de7bc17b992f72ec1c984e078268bddd
sunhillau <sunhill.au@gmail.com>
2015-08-10 22:46:53 +1000

    Basic Setup
    Basic Setup

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Title</title>
+  <link rel="shortcut icon" href="images/favicon.ico" />
+  <meta name="description" content="content" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<body>
+<!--[if lt IE 9]>
+<p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<header>
+<!--==================================
+		Mobile Header
+====================================-->
+<nav>
+	<
+</nav>
+</header>
