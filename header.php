3d74f9528d71f724473f62f2d501df386a24febf
sunhillau <sunhill.au@gmail.com>
2015-08-11 17:04:54 +1000

    Updates 11082015

--- header.php
+++ header.php
@@ -14,12 +14,47 @@
 <!--[if lt IE 9]>
 <p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
-<header>
+<header><a name="homepage"></a>
 <!--==================================
 		Mobile Header
 ====================================-->
-	<nav class="nav_container">
-		<div class="logo_img"></div>
+		
+	<section class="nav_container">
+		<div class="tool_bar cevc">
+			<img src="images/demo/logo.png" alt="logo" />
+			<div><i class="fa fa-bars" id="menu_icon"></i></div>
+		</div>
+		<div class="background_blur"></div>
+		<!-- Navigation Menu -->
+		<nav class="menu_overlay">
+			<ul class="unlist_style">
+				<li><a href="#homepage">Home</a></li>
+				<li><a href="#">Who We Are</a></li>
+				<li><a href="#">Our Work</a></li>
+				<li><a href="#">Testimonial</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		</nav>
+		
+		<div class="tool_bar_clearfix"></div>
+		
+		<div class="head_text">
+			<h2>YC BUILDING</h2>
+			<h3>Trusted Canberra Based Contractor & Builder</h3>
+		</div>
+		
+		<div class="tool_bar_clearfix_2"></div>
+		<div class="under_bk cevc">
+			<button>OUR WORK</button><button>GET QUOTES</button>
+		</div>
 
-	</nav>
+
+	</section>
+<div class="grey_overlay"></div> <!-- overlay active -->
 </header>
+
+<!-- Page 2 -->
+<seciont>
+<h2>What We Do</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</seciont>
---
+++ js/js_main.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+	$('#menu_icon').click(function(){
+		$('.menu_overlay').toggleClass('menu_overlay_push');
+	});
+});
